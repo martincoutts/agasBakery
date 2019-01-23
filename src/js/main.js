38a31c7787d47a8573a9d730b09a5960a998d4c6
martincavaliers <martincoutts5@gmail.com>
2019-01-23 19:15:03 +0000

    Arrow button functionality now working

--- src/js/main.js
+++ src/js/main.js
@@ -1,43 +1,18 @@
-function scroller() {
-  $('a[href*="#"]').on("click", function(e) {
-    e.preventDefault();
-
-    $("html, body").animate(
-      {
-        scrollTop: $($(this).attr("href")).offset().top
-      },
-      400,
-      "linear"
-    );
+function arrowFadeIn() {
+  $(document).scroll(function() {
+    var y = $(this).scrollTop();
+    if (y > 800 && windowWidth > 991) {
+      $(".arrow-button").fadeIn(500);
+    } else {
+      $(".arrow-button").fadeOut(200);
+    }
   });
 }
 
-const desktopView = false;
+let windowWidth = window.innerWidth;
 
-$(window).resize(function(){
-  if($(window).width() >= 992){
-    desktopView === true;
-  }else{
-    desktopView === false;
-  }
+$(window).resize(function() {
+  windowWidth = window.innerWidth;
 });
 
-$(document).ready(function() {
-  
-  // Arrow button fade in
-  if($(window).width() >= 992){
-    desktopView === true;
-  }
-  if(desktopView === true){
-  $(document).on('scroll', function() {
-    $(document).scroll(function() {
-      var y = $(this).scrollTop();
-      if (y > 800) {
-        $('.arrow-button').fadeIn(500);
-      } else {
-        $('.arrow-button').fadeOut(200);
-      }
-    });
-})
-  }
-});
+arrowFadeIn();
